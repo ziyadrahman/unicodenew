2f110c346e5cb8af0d4583f5e6864150394a0448
jishnu m <mjishnu990@gmail.com>
2024-12-18 15:57:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['https://unicode-mu.vercel.app', 'http://localhost:3000'],
+    origin: ['https://unicode-mu.vercel.app', 'http://localhost:3000','https://unicode-two.vercel.app'],
     credentials: true,
   })
 );
